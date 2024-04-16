25e13ce3c06f8b077076503ae256a0a838328019
PSriVignesH <srivigneshsri1999@gmail.com>
2024-04-16 15:48:42 +0530

    fixing loader

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const Loading = () => {
   return (
-  <div className='h-full flex flex-col items-center justify-center'>
+  <div className='h-full flex  items-center justify-center'>
     <Loader />
   </div>
   )
